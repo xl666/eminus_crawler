54be217c6fa6b1300f6adb98eb2268f7f7522443
xl666 <xavier120@hotmail.com>
2019-06-11 12:43:12 -0500

    -Se agrega rutina para extraer todas las respuestas de alumnos  de una actividad
    -Se modifica la rutina que regresa los alumnos que contestaron a
     una actividad para que funcione con un generador y evite referencias
     stale (a costa de repetir un lookup)

--- actividades.py
+++ actividades.py
@@ -4,11 +4,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import requests
+from collections import namedtuple
+import os
+import copy
+
 
 URL_ACTIVIDADES = 'https://eminus.uv.mx/eminus/actividades/centroActividades.aspx'
 URL_ACTIVIDADES_ALUMNOS = 'https://eminus.uv.mx/eminus/Evaluacion/IntegrantesActividades.aspx'
 URL_ACTIVIDAD_DETALLE = 'https://eminus.uv.mx/eminus/actividades/RevisionActividad.aspx'
 URL_DESCARGA_RESPUESTA = 'https://eminus.uv.mx/eminus/Recursos.aspx?id=%s&tipo=1'
+
+Enlace = namedtuple('Enlace', 'nombre url')
 
 def ir_a_actividades(driver):
     assert driver.current_url == cursos.URL_MAIN
@@ -53,20 +59,26 @@ def ir_a_actividad(driver, actividades, pk):
 
 def regresar_alumnos_contestaron_actividad(driver):
     """
-    Regresa los elemenentos asociados a alumnos que contestaron a la actividad
-    el diccionario de retorno es matricula: elemento
+    Regresa un generador con los elemenentos asociados a alumnos que contestaron a la actividad
+    Tiene acoplamiento semantico con extraer_respuestas_actividad
+    Necesita que despues de cada yield se regrese a la pagina URL_ACTIVIDADES_ALUMNOS
+    Probablemente sea la forma mas eficiente de implementar
     """
     assert driver.current_url == URL_ACTIVIDADES_ALUMNOS
-    alumnos = driver.find_elements_by_class_name('DivContenedorDatos')
     respuesta = {}
-    for alumno in alumnos:
-        if not 'rgb(0, 0, 51)' in alumno.find_element_by_tag_name('label').get_attribute("style"):
-            respuesta[alumno.get_attribute('id')] = alumno
-    return respuesta
+    i = 0
+    while True:
+        # Es necesario hacerlo asi para evitar referencias stale
+        alumnos = driver.find_elements_by_class_name('DivContenedorDatos')
+        if i >= len(alumnos):
+            break
+        if not 'rgb(0, 0, 51)' in alumnos[i].find_element_by_tag_name('label').get_attribute("style"):
+            yield alumnos[i].get_attribute('id'), alumnos[i]
+        i += 1
 
-def ir_a_respuesta_alumno(driver, matricula, alumnos):
+def ir_a_respuesta_alumno(driver, alumno):
     assert driver.current_url == URL_ACTIVIDADES_ALUMNOS
-    alumnos.get(matricula).click()
+    alumno.click()
     try:
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'contenedorIntegranteEnc')))
@@ -82,14 +94,17 @@ def regresar_texto_respuesta_alumno(driver):
 def regresar_enlaces_archivos_respuesta_alumno(driver):
     assert driver.current_url == URL_ACTIVIDAD_DETALLE
     enlaces = []
-    entrega = driver.find_element_by_id('sctMaterialEstudiante')
+    try:
+        entrega = driver.find_element_by_id('sctMaterialEstudiante')
+    except:
+        return []
     descargas = entrega.find_elements_by_class_name('filaArchivoDescarga')
     nombres = entrega.find_elements_by_class_name('filaArchivoNombre')
     for info in zip(nombres, descargas):
         # el formato se ve como: descargarArchivo(1,2680088,2)
         detalles = info[1].get_attribute('onclick').split(',')
-        enlaces.append((info[0].get_attribute("textContent"),
-                       URL_DESCARGA_RESPUESTA % detalles[1]))
+        enlaces.append(Enlace(info[0].get_attribute("textContent"),
+                              URL_DESCARGA_RESPUESTA % detalles[1]))        
     return enlaces
 
 def guardar_enlace(driver, enlace, ruta):
@@ -117,4 +132,26 @@ def extraer_respuestas_actividad(driver, actividades, pk, ruta_salida):
     """
     Guarda todos los recursos de una actividad creando una estructura de directorios en ruta
     """
-    pass
+    assert driver.current_url == URL_ACTIVIDADES
+    ir_a_actividad(driver, actividades, pk)
+    for matricula, alumno in regresar_alumnos_contestaron_actividad(driver):
+        ruta_alumno = '%s/%s' % (ruta_salida, matricula)
+        try:            
+            os.mkdir(ruta_alumno)
+        except FileExistsError:
+            if not os.path.isdir(ruta_alumno):
+                raise Exception('No se puede crear directorio para guardar archivos de alumno, la ruta ya existe y no es directorio:%s' % ruta_alumno)
+        except Exception:
+            raise Exception('No se puede crear directorio para guardar archivos de alumno')
+            
+        ir_a_respuesta_alumno(driver, alumno)
+        texto = regresar_texto_respuesta_alumno(driver)
+        enlaces = regresar_enlaces_archivos_respuesta_alumno(driver)
+        guardar_actividad_alumno(driver, texto, enlaces, ruta_alumno)
+        driver.back()
+        driver.refresh()
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.text_to_be_present_in_element((By.ID, 'EtiquetaTitulo1'), 'Actividad'))
+        except:
+            raise Exception('No se puede acceder a la actividad solicitada')
